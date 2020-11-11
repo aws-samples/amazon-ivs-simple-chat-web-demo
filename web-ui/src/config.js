a18a745c5016468d8d5ef5c23e71f5dda0a772a6
Maxime Boulin <boulinm@amazon.com>
2020-11-11 08:02:01 -0800

    cleanup & dependencies update

--- web-ui/src/config.js
+++ web-ui/src/config.js
@@ -1,9 +1,9 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-// Default video stream to play inside the video player
+// Amazon IVS Playback URL
 // Replace this with your own Amazon IVS Playback URL
 export const PLAYBACK_URL = "https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.DmumNckWFTqz.m3u8";
 
-// Default Chat websocket link
+// Chat websocket address
 export const CHAT_WEBSOCKET = "";
